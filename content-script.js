332668ac8fa47ddee51a0071835b787f20ef314a
Paul Tanasă <pebly@tuta.io>
2023-12-12 10:55:07 +0200

    fix: fallback for inexact likes count

--- content-script.js
+++ content-script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let ReturnInstagramLikes__Cache = {};
+let cache = {};
 
 async function ReturnInstagramLikes(id) {
-  if (ReturnInstagramLikes__Cache[id] !== undefined) {
-    return ReturnInstagramLikes__Cache[id];
+  if (cache[id] !== undefined) {
+    return cache[id];
   }
 
   const response1 = await fetch(`https://www.instagram.com/p/${id}`, {
@@ -27,7 +27,7 @@ async function ReturnInstagramLikes(id) {
     `https://www.instagram.com/api/v1/media/${match(
       /"media_id":"(.*?)"/g,
       body1
-    )}/likers`,
+    )}/likers/`,
     {
       credentials: 'include',
       headers: {
@@ -46,12 +46,13 @@ async function ReturnInstagramLikes(id) {
     body2 = await response2.json();
   } catch (_) {}
 
-  ReturnInstagramLikes__Cache[id] = {
+  cache[id] = {
     count: body2.user_count || null,
-    id
+    id,
+    length: body2.users.length
   };
 
-  return ReturnInstagramLikes__Cache[id];
+  return cache[id];
 }
 
 setInterval(async () => {
@@ -82,7 +83,7 @@ setInterval(async () => {
     section.insertAdjacentHTML(
       'afterbegin',
       `
-        <span id="ReturnInstagramLikes-${id}" style="align-items: center; background: linear-gradient(30deg, #f9ce34 0%, #ee2a7b 50%,#6228d7 100%); border-radius: 999px; color: #fff; cursor: pointer; display: flex; gap: 4px; font-size: 16px; font-weight: 400; height: fit-content; margin: 4px 16px 4px 0; min-height: 32px; padding: 0 8px; user-select: none; width: fit-content;">
+        <span id="ReturnInstagramLikes-${id}" style="align-items: center; background: linear-gradient(30deg, #f9ce34 0%, #ee2a7b 50%, #6228d7 100%); border-radius: 999px; color: #fff; cursor: pointer; display: flex; gap: 4px; font-size: 16px; font-weight: 400; height: fit-content; margin: 4px 16px 4px 0; min-height: 32px; padding: 0 8px; user-select: none; width: fit-content;">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
@@ -90,9 +91,14 @@ setInterval(async () => {
           <span style="color: inherit; display: none; font-size: 14px; font-weight: 600;">
             0
           </span>
-          <b style="color: inherit; font-size: 10px; font-weight: 600; text-transform: uppercase;">
-            click to load
-          </b>
+          <div style="color: inherit; display: flex; flex-direction: column; font-weight: 600; gap: 2px; text-transform: uppercase;">
+            <b style="font-size: 10px; line-height: 100%;">
+              click to load
+            </b>
+            <span style="display: none; font-size: 8px; font-style: italic; line-height: 100%;">
+              not sure
+            </span>
+          </div>
         </span>
       `
     );
@@ -106,28 +112,37 @@ setInterval(async () => {
 
       span.style.cursor = '';
 
-      const b = span.querySelector('b');
-
       const count = span.querySelector('span');
 
-      b.textContent = 'loading...';
+      const divB = span.querySelector('div > b');
+
+      const divSpan = span.querySelector('div > span');
+
+      divB.textContent = 'loading...';
 
       const likes = await ReturnInstagramLikes(id);
 
       if (likes.count === null) {
-        b.textContent = 'error';
+        divB.textContent = 'error';
       } else {
-        b.textContent = 'likes';
+        divB.textContent = 'likes';
 
         count.style.display = 'flex';
 
-        count.textContent = likes.count
-          .toString()
-          .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        count.textContent = format(
+          likes.count < likes.length ? likes.length : likes.count
+        );
+
+        if (likes.count < likes.length) {
+          divSpan.style.display = 'flex';
+        }
       }
     };
 
-    if (ReturnInstagramLikes__Cache[id] === undefined) {
+    const format = (number) =>
+      number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    if (cache[id] === undefined) {
       span.addEventListener('click', compute);
     } else {
       await compute();
